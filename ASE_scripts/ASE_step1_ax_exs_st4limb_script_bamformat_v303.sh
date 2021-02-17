a57ad05e67a8f636f6c4a96de4d34c4961ae0024
Andrea Berardi <aeberardi@users.noreply.github.com>
2021-02-17 13:33:46 -0500

    Update ASE_step1_ax_exs_st4limb_script_bamformat_v303.sh
    updated script for public use

--- ASE_scripts/ASE_step1_ax_exs_st4limb_script_bamformat_v303.sh
+++ ASE_scripts/ASE_step1_ax_exs_st4limb_script_bamformat_v303.sh
@@ -1,54 +1,54 @@
 #!/bin/bash
-#SBATCH --mail-user=andrea.berardi@ips.unibe.ch
+#SBATCH --mail-user=youremail@wherever.edu
 #SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=60G
-#SBATCH --workdir=/home/ubelix/ips/berardi/RNAseq_axex/mapped_to_ax303/ASE
+#SBATCH --workdir=/your/working/directory
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH -J "ASE step 1"
 
 module add vital-it/7
-#module add UHTS/Analysis/picard-tools/1.130;
 module add UHTS/Analysis/picard-tools/2.18.11
-#module add Development/java_jdk/1.8.0_112;
-
-#after https://wiki.systemsx.ch/display/~tural.yarahmadov@ips.unibe.ch/RNA-seq+data+analysis+pipeline
 
 
-#Tural helped me to understand this code. Process each individual bamfile through the first four steps, then process them per-group through the last two of this pipeline.
 
-#Making symbolic links for this analysis so that I can use an array command
+#Step 1: organize bam files (make sure they are soorted by coord) and give symbolic links with numbers so you can use SLURM arrays
+mkdir ASE
+cd ASE
+
+### ---- Making symbolic links for this analysis so that I can use an array command ---- ###
+#usage#
 #ln -s source_file symboliclinknameforfile
 
+#list of samples (3 parents species 1, 3 parents species 2, 3 F1 hybrids)
 #ax_exF1Limb1_Aligned.sortedByCoord.out.bam  ax_Limb3_Aligned.sortedByCoord.out.bam
 #ax_exF1Limb2_Aligned.sortedByCoord.out.bam  exs_Limb1_Aligned.sortedByCoord.out.bam
 #ax_exF1Limb3_Aligned.sortedByCoord.out.bam  exs_Limb2_Aligned.sortedByCoord.out.bam
 #ax_Limb1_Aligned.sortedByCoord.out.bam      exs_Limb3_Aligned.sortedByCoord.out.bam
 #ax_Limb2_Aligned.sortedByCoord.out.bam
 
-#F1 group
-    #/home/ubelix/ips/berardi/RNAseq_axex/ASE
+#F1 group, name them 4-6
 #    ln -s ../ax_exF1Limb1_Aligned.sortedByCoord.out.bam 4.sortedByCoord.out.bam
 #    ln -s ../ax_exF1Limb2_Aligned.sortedByCoord.out.bam 5.sortedByCoord.out.bam
 #    ln -s ../ax_exF1Limb3_Aligned.sortedByCoord.out.bam 6.sortedByCoord.out.bam
         
-#axillaris group
+#axillaris group, name them 1-3 (parent species 1)
 #    ln -s ../ax_Limb1_Aligned.sortedByCoord.out.bam 1.sortedByCoord.out.bam
 #    ln -s ../ax_Limb2_Aligned.sortedByCoord.out.bam 2.sortedByCoord.out.bam
 #    ln -s ../ax_Limb3_Aligned.sortedByCoord.out.bam 3.sortedByCoord.out.bam
 
-#exserta group
+#exserta group, name them 7-9 (parent species 2)
 #    ln -s ../exs_Limb1_Aligned.sortedByCoord.out.bam 7.sortedByCoord.out.bam
 #    ln -s ../exs_Limb2_Aligned.sortedByCoord.out.bam 8.sortedByCoord.out.bam
 #    ln -s ../exs_Limb3_Aligned.sortedByCoord.out.bam 9.sortedByCoord.out.bam
     
     
 
-#I also made a symbolic link (or copied if space) to the reference genome fasta file in this directory, Petunia_axillaris_v1.6.2_genome.fasta
-#ln -s /home/ubelix/ips/berardi/axillaris/Paxillaris_v303/peaxi162AQ_PeaxHIC303.cds.swapphase.gff peaxi162AQ_PeaxHIC303.cds.swapphase.gff
-#ln -s /home/ubelix/ips/berardi/axillaris/Paxillaris_v303/Petunia_axillaris.v3.0.3.fasta Petunia_axillaris.v3.0.3.fasta
+#I also made a symbolic link (or copied if space) to the reference genome fasta file and gff annotation in this directory
+#ln -s /directory_w_genomefiles/peaxi162AQ_PeaxHIC303.cds.swapphase.gff peaxi162AQ_PeaxHIC303.cds.swapphase.gff
+#ln -s /directory_w_genomefiles/Petunia_axillaris.v3.0.3.fasta Petunia_axillaris.v3.0.3.fasta
 
 
-##### things to do beforehand #####
+##### things to do before executing #####
 
 #make a temp folder in your working directory if you don't have one already; https://gatkforums.broadinstitute.org/gatk/discussion/6083/picard-tools-markduplicates-spilling-to-disk
 #mkdir ./temp
@@ -62,7 +62,7 @@ module add UHTS/Analysis/picard-tools/2.18.11
 
 #make sure that your bamfiles have read group IDs: https://software.broadinstitute.org/gatk/documentation/article?id=1317
 #samtools view -H /path/to/my.bam | grep '^@RG'
-#if not, can use picard-tools AddOrReplaceReadGroups, where As long as the RGSM is different for each of your samples, things should work
+#if not, can use picard-tools AddOrReplaceReadGroups. As long as the RGSM is different for each of your samples, things should work.
 picard-tools AddOrReplaceReadGroups RGLB=Lane1 RGPL=Illumina RGPU=TTAGGC RGSM=exs1 I=7.sortedByCoord.out.bam O=7.sortedByCoord.rg.out.bam
 picard-tools AddOrReplaceReadGroups RGLB=Lane1 RGPL=Illumina RGPU=TTAGGC RGSM=exs2 I=8.sortedByCoord.out.bam O=8.sortedByCoord.rg.out.bam
 picard-tools AddOrReplaceReadGroups RGLB=Lane1 RGPL=Illumina RGPU=TTAGGC RGSM=exs3 I=9.sortedByCoord.out.bam O=9.sortedByCoord.rg.out.bam
