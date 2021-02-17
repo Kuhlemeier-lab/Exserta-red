adcb04297c8fca8efb2009417680142a786f29f1
Andrea Berardi <aeberardi@users.noreply.github.com>
2021-02-17 13:47:24 -0500

    Update ASE_step4_script_ase_axexsF1samples_v303.sh
    updated for public use

--- ASE_scripts/ASE_step4_script_ase_axexsF1samples_v303.sh
+++ ASE_scripts/ASE_step4_script_ase_axexsF1samples_v303.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --mail-user=andrea.berardi@ips.unibe.ch
+#SBATCH --mail-user=youremail@whatever.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --workdir=/home/ubelix/ips/berardi/RNAseq_axex/mapped_to_ax303/ASE
+#SBATCH --workdir=/yourdirectory/ASE
 #SBATCH --time=8:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --array=4-6
@@ -13,16 +13,18 @@ module add UHTS/Analysis/picard-tools/2.18.11
 module add UHTS/Analysis/GenomeAnalysisTK/3.5.0
 module add UHTS/Analysis/samtools/1.8
 
+### ---- Now that we have the variants called (exserta variants against an axillaris reference) we use GATK to count allele-specific expression in the F1 hybrid samples ---- ###
+mkdir ASEcounts
 
-#F1 samples
+#F1 samples separately
 #counting reads surrounding SNPs with prev generated filters for each sample
 GenomeAnalysisTK -T ASEReadCounter -R Petunia_axillaris.v3.0.3.fasta -I ${SLURM_ARRAY_TASK_ID}.re.bam -o ./ASEcounts/${SLURM_ARRAY_TASK_ID}.strict.counts -U ALLOW_N_CIGAR_READS -sites Pexs_st4_biallelic_snps_selected_strict.vcf -mmq 50 -minDepth 20
 
-#NOW do one run with all combined for a sort of "total" count file
+#Now do one run with all F1 samples combined for a "total" count file
 GenomeAnalysisTK -T ASEReadCounter -R Petunia_axillaris.v3.0.3.fasta -I 4.re.bam -I 5.re.bam -I 6.re.bam -o ./ASEcounts/F1s.strict.counts -U ALLOW_N_CIGAR_READS -sites Pexs_st4_biallelic_snps_selected_strict.vcf -mmq 50 -minDepth 20
 
 
 #rosetta stone if needed
-#1-3 ax 1-3
+#1-3 axillaris 1-3
 #4-6 f1 1-3
-#6-9 exs 1-3
+#6-9 exserta 1-3
